fd7a35fe6b13f37523e0bbb8c99267edbfd67b40
nurbek <nisagaliv035gmail.com>
2021-09-02 22:01:54 +0600

    CREATION  FOOTER,HEADER,DEFAULT,ABOUVE

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TheHeader />
+    <Nuxt />
+    <TheFooter />
+  </div>
+</template>
+<script>
+</script>
+<style>
+</style>
---
+++ frontend/pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>asdasdfadsf</p>
+</template>
